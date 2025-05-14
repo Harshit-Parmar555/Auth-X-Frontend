d4ce5b7a9832b5e22aacb48379ba1f28191113a0
Harshit_Parmar <parmarharshit441@gmail.com>
2025-05-15 00:53:00 +0530

    feat: add verify email page and configure route

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 // pages import
 import Land from "./pages/Land/Land";
 import Register from "./pages/Register/Register";
+import Verify from "./pages/Verify/Verify";
 import Login from "./pages/Login/Login";
 
 const App = () => {
@@ -13,6 +14,7 @@ const App = () => {
         <Route path="/" element={<Land />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/verify-email" element={<Verify />} />
       </Routes>
     </BrowserRouter>
   );
