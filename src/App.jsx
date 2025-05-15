c43fce2fb0c337d7d340ad718ef58fe94b2bf3a2
Harshit_Parmar <parmarharshit441@gmail.com>
2025-05-15 10:43:55 +0530

    feat: set up protected and authenticated user middleware routes in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 // pages import
 import Land from "./pages/Land/Land";
@@ -14,6 +14,30 @@ import Terms from "./pages/T&P/Terms";
 import Privacy from "./pages/T&P/Privacy";
 
 import { Toaster } from "react-hot-toast";
+
+// store import
+import { useAuthStore } from "./store/useAuthStore.js";
+
+// protected route
+const ProtectedRoute = ({ children }) => {
+  const { isAuthenticated, user } = useAuthStore();
+  if (!isAuthenticated) {
+    return <Navigate to="/login" replace />;
+  }
+  if (!user.isVerified) {
+    return <Navigate to="/verify-email" replace />;
+  }
+  return children;
+};
+
+//  authenticated route
+const RedirectAuthenticatedUser = ({ children }) => {
+  const { user, isAuthenticated } = useAuthStore();
+  if (isAuthenticated && user.isVerified) {
+    return <Navigate to="/dashboard" replace />;
+  }
+  return children;
+};
 
 const App = () => {
   return (
@@ -44,15 +68,78 @@ const App = () => {
         }}
       />
       <Routes>
-        <Route path="/" element={<Land />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/verify" element={<Verify />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<DashBoard />} />
-        <Route path="/forgetpassword" element={<Forgot />} />
-        <Route path="/resetpassword/:token" element={<Reset />} />
-        <Route path="/terms" element={<Terms />} />
-        <Route path="/privacy" element={<Privacy />} />
+        <Route
+          path="/"
+          element={
+            <RedirectAuthenticatedUser>
+              <Land />
+            </RedirectAuthenticatedUser>
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <RedirectAuthenticatedUser>
+              <Register />
+            </RedirectAuthenticatedUser>
+          }
+        />
+        <Route
+          path="/verify"
+          element={
+            <RedirectAuthenticatedUser>
+              <Verify />
+            </RedirectAuthenticatedUser>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <RedirectAuthenticatedUser>
+              <Login />
+            </RedirectAuthenticatedUser>
+          }
+        />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <DashBoard />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/forgetpassword"
+          element={
+            <RedirectAuthenticatedUser>
+              <Forgot />
+            </RedirectAuthenticatedUser>
+          }
+        />
+        <Route
+          path="/resetpassword/:token"
+          element={
+            <RedirectAuthenticatedUser>
+              <Reset />
+            </RedirectAuthenticatedUser>
+          }
+        />
+        <Route
+          path="/terms"
+          element={
+            <RedirectAuthenticatedUser>
+              <Terms />
+            </RedirectAuthenticatedUser>
+          }
+        />
+        <Route
+          path="/privacy"
+          element={
+            <RedirectAuthenticatedUser>
+              <Privacy />
+            </RedirectAuthenticatedUser>
+          }
+        />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
